af0fd2c5f939830b54b94cb87167ea6bd9df81b0
buihuykhang2006 <buihuykhang2006@gmail.com>
2026-06-08 21:47:39 +0700

    chỉnh sữa Nhân Viên

--- Quanlythuvien/DAL/NhanVienDAL.cs
+++ Quanlythuvien/DAL/NhanVienDAL.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Data.SqlClient;
 using QuanLyThuVien.DTO;
 
 namespace QuanLyThuVien.DAL
@@ -7,24 +8,42 @@ namespace QuanLyThuVien.DAL
     {
         public string KiemTraDangNhap(string taiKhoan, string matKhau)
         {
-            string query = string.Format("SELECT QuyenHan FROM NhanVien WHERE TenDangNhap = '{0}' AND MatKhau = '{1}'", taiKhoan, matKhau);
-            DataTable dt = SqlHelper.ExecuteQuery(query);
+            string query = "SELECT QuyenHan FROM NhanVien WHERE TenDangNhap = @tk AND MatKhau = @mk";
+            SqlParameter[] p = {
+                new SqlParameter("@tk", taiKhoan),
+                new SqlParameter("@mk", matKhau)
+            };
+
+            DataTable dt = SqlHelper.ExecuteQuery(query, p);
             if (dt.Rows.Count > 0) return dt.Rows[0]["QuyenHan"].ToString();
             return "";
         }
 
         public bool KiemTraTrungTaiKhoan(string tenDangNhap)
         {
-            string query = string.Format("SELECT COUNT(*) FROM NhanVien WHERE TenDangNhap = '{0}'", tenDangNhap);
-            DataTable dt = SqlHelper.ExecuteQuery(query);
+            string query = "SELECT COUNT(*) FROM NhanVien WHERE TenDangNhap = @tk";
+            SqlParameter[] p = { new SqlParameter("@tk", tenDangNhap) };
+
+            DataTable dt = SqlHelper.ExecuteQuery(query, p);
+            // Kiểm tra nếu số lượng trả về > 0 thì tài khoản đã tồn tại
             return int.Parse(dt.Rows[0][0].ToString()) > 0;
         }
 
         public bool DangKy(NhanVienDTO nv)
         {
-            string query = string.Format("INSERT INTO NhanVien VALUES ('{0}', N'{1}', '{2}', N'{3}', '{4}', '{5}')",
-                nv.MaNhanVien, nv.HoTen, nv.SoDienThoai, nv.QuyenHan, nv.TenDangNhap, nv.MatKhau);
-            return SqlHelper.ExecuteNonQuery(query);
+            string query = @"INSERT INTO NhanVien (MaNhanVien, HoTen, SoDienThoai, QuyenHan, TenDangNhap, MatKhau) 
+                             VALUES (@ma, @ten, @sdt, @quyen, @tk, @mk)";
+
+            SqlParameter[] p = {
+                new SqlParameter("@ma", nv.MaNhanVien),
+                new SqlParameter("@ten", nv.HoTen),
+                new SqlParameter("@sdt", nv.SoDienThoai),
+                new SqlParameter("@quyen", nv.QuyenHan),
+                new SqlParameter("@tk", nv.TenDangNhap),
+                new SqlParameter("@mk", nv.MatKhau)
+            };
+
+            return SqlHelper.ExecuteNonQuery(query, p);
         }
     }
 }
